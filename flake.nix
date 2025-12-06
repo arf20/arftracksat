ba04572bb7ed03fde6aac9d79c0bbc34939bab18
Pol <55996847+pol-epsevg@users.noreply.github.com>
2025-12-06 11:33:16 +0100

    Update flake.nix (#44)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A graphical satellite tracking software for linux";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-24.05;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-25.11;
     flake-utils.url = github:numtide/flake-utils;
   };
 
